4326a352948e485f068a87a5f4fa709fddb00159
arberimbibaj <arberim.bibaj98@hotmail.com>
2023-07-01 03:18:24 +0200

    new

---
+++ HelperFuctions.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+
+
+def first_step():
+    pass
+
+
+def second_step():
+    pass
+
+
+def trainer():
+    pass
+
+
+def predictor():
+    pass
+
+
+class NNRegression:
+    pass
+
+
+class NNClassification:
+    pass
