309bf1673dcbd2cc059735892144737c360fba06
arberimbibaj <arberim.bibaj98@hotmail.com>
2023-07-02 03:31:08 +0200

    added pseudo outcomes function

--- HelperFuctions.py
+++ HelperFuctions.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import numpy as np
 import tensorflow as tf
 
@@ -18,9 +20,28 @@ def predictor():
     pass
 
 
-class NNRegression:
+class NeuralNetworkModel:
     pass
 
 
 class NNClassification:
     pass
+
+
+def r_u_pseudo_outcomes(y, w, probs, mu, eps):
+    return (y - mu) / (w - probs + eps)
+
+
+def dr_pseudo_outcomes(y, w, probs, mu0, mu1, eps):
+    mu_w = w * mu1 + (1 - w) * mu0
+    return (w - probs) / (probs * (1 - probs) + eps) * (y - mu_w) + mu1 - mu0
+
+
+def ra_pseudo_outcomes(y, w, mu0, mu1):
+    return w * (y - mu0) + (1 - w) * (mu1 - y)
+
+
+def pw_pseudo_outcomes(y, w, probs, eps):
+    return (w / (probs + eps) - (1 - w) / ((1 - probs) + eps)) * y
+
+
