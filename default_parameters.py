947e69585f2d29183aad4b5d1725ad211210ef3e
arberimbibaj <arberim.bibaj98@hotmail.com>
2023-08-31 21:04:28 +0200

    for cluster run 31 August

--- default_parameters.py
+++ default_parameters.py
@@ -6,18 +6,17 @@
 KERAS_SEED: int = 2023  # keras seed
 R_SEED: int = 2023  # r seed
 NP_SEED: int = 2023  # numpy seed
-RANDOM: int = 2023  # random_state for random forest and linear models
+RANDOM: None  # random_state for random forest and linear models # TODO: change?
 
 # Clip values for the predicted propensities
-MIN_CLIP: float = 0.05  # TODO: changed was 0.01
-MAX_CLIP: float = 0.95  # TODO: changed was 0.99
+MIN_CLIP: float = 0.05
+MAX_CLIP: float = 0.95
 
 # data generation
 TEST_SIZE: int = 1000
 SAMPLE_SIZES: list = [500, 1000, 2000, 5000]
 N_RUNS: int = 10
 DIMENSION: int = 20  # Dimension of X
-N_SETUPS: int = 24  # the 18 different settings, TODO:  NOT NEEDED!
 
 ''''''' NEURAL NETWORKS '''''''
 
@@ -25,22 +24,21 @@ N_SETUPS: int = 24  # the 18 different settings, TODO:  NOT NEEDED!
 NON_LINEARITY: str = 'elu'
 
 # EARLY STOPPING
-PATIENCE: int = 5  # TODO: changed was 10 when tried.
-START_FROM: int = 50  # TODO: changed was 200 when tried
-VALIDATION_SPLIT: float = 0.3
+PATIENCE: int = 5  # changed was 10 when tried.
+START_FROM: int = 50
+VALIDATION_SPLIT: float = 0.1  # TODO: THIS!!!
 
 # OPTIMIZER
 BATCH_SIZE: int = 100
-WEIGHT_DECAY: float = 1e-3  # TODO: changed, was 1e-4, when tried
-N_EPOCHS: int = 100  # TODO: change, was 500
+WEIGHT_DECAY: float = 1e-3  # changed, was 1e-4, when tried
+N_EPOCHS: int = 100
 LEARNING_RATE: float = 1e-4
-LABEL_SMOOTHING: float = 0.0  # TODO: changed, was 0.1
+LABEL_SMOOTHING: float = 0.0  # 0 means no smoothing, changed, was 0.1
 
 ''''''' RANDOM FORESTS '''''''
 N_TREES: int = 1000
 MAX_DEPTH: int = 7
 MAX_FEATURES: float = 0.33
-# justified in: https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
 
 
 ''''''' LASSO '''''''
